40aed5c126bb45272d570d8b6f1ba1a5e64a4173
blackranja <josphatranja243@gmail.com>
2023-09-15 23:57:33 +0300

    Job-Details Update

--- jobs/components/jobdetails/company/Company.jsx
+++ jobs/components/jobdetails/company/Company.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import styles from './company.style'
-
-const Company = () => {
+import {icons} from '../../../constants';
+import { checkImageURL } from '../../../utils';
+const Company = ({companyLogo,jobTitle,companyName,location}) => {
   return (
-    <View>
-      <Text>Company</Text>
+   <View style={styles.container}>
+    <View style={styles.logoBox}>
+      <Image
+      source={{
+        url:checkImageURL(companyLogo)
+        ? companyLogo
+        : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+      }}
     </View>
+   </View>
   )
 }
 
