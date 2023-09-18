778acf4e20c4d1181fe9af06128ecaa58bfd49e6
blackranja <josphatranja243@gmail.com>
2023-09-18 11:37:49 +0300

    Tabs Button Framework

--- jobs/components/jobdetails/tabs/Tabs.jsx
+++ jobs/components/jobdetails/tabs/Tabs.jsx
@@ -3,13 +3,36 @@ import { View, Text,TouchableOpacity,FlatList } from 'react-native'
 
 import styles from './tabs.style'
 import {SIZES} from '../../../constants';
-const TabButton=()=>{
+const TabButton=({name,activeTab,onHandleSearchType})=>{
+  <TouchableOpacity
+  style={styles.btn(name,activeTab)}
+  onPress={onHandleSearchType}
+  >
+    <Text style={style.btnText(name,activeTab)}>
+      {name}s
+    </Text>
+  </TouchableOpacity>
 
 }
-const Tabs = ({tabs,activeTabs,setActiveTab}) => {
+const Tabs = ({tabs,activeTab,setActiveTab}) => {
   return (
    <View style={styles.container}>
     <FlatList 
+    data={data}
+    renderItem={({item})=>(
+      <TabButton
+      name={item}
+      activeTab={activeTab}
+      onHandleSearchType={()=>setActiveTab({item})}
+      />
+
+    )
+    }
+    horizontal
+    showsHorizontalScrollIndicator={false}
+    keyExtractor={item =>item}
+    constentContainerStyle={{columnGap:SIZES.small / 2 }}
+    />
    </View>
   )
 }
